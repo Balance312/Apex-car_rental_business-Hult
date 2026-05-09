56457eefb2480ca2cf54dbff96d6d72a6cf78047
Abdullah sherif <Abdollah.Mohamed.FCI2024250@sadatacademy.edu.eg>
2026-05-09 09:57:09 +0300

    Creating the Apex website for car rental business.

---
+++ contact-us.js
@@ -0,0 +1,13 @@
+document.getElementById("form-message").style.display = "none";
+document.getElementById("scroll-to-form").addEventListener("click", function () {
+    document.getElementById("form").scrollIntoView({ behavior: "smooth", block: "start" });
+});
+if (document.getElementById("form")) {
+    document.getElementById("form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        document.getElementById("form-message").style.display = "block";
+        document.getElementById("form-message").textContent = "Your request has been submitted successfully!";
+        document.getElementById("form").reset();
+        document.getElementById("form-message").style.color = "green";
+    });}
+    
