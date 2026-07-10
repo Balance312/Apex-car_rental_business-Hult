501577b81980f480f99e590ba7e496d1591212e9
Balance312 <187170468+Balance312@users.noreply.github.com>
2026-07-10 16:20:43 +0300

    refactor navigation links and enhance reserve button functionality

--- contact-us.js
+++ contact-us.js
@@ -11,3 +11,12 @@ if (document.getElementById("form")) {
         document.getElementById("form-message").style.color = "green";
     });}
     
+
+const reserveButton = document.getElementById("reserve-btn");
+
+if (reserveButton) {
+	reserveButton.addEventListener("click", function (event) {
+		event.preventDefault();
+		alert("Sorry, this feature is not working at the moment.");
+	});
+}
